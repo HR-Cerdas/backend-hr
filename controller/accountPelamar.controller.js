b80b6efa8af10c5f0449b8507b03d14f5935d579
geofannyher <67730049+geofannyher@users.noreply.github.com>
2022-10-28 08:56:50 +0700

    hehehe

--- controller/accountPelamar.controller.js
+++ controller/accountPelamar.controller.js
@@ -137,7 +137,7 @@ const login = async (req, res, next) => {
       const token = signToken(payload);
       return res.status(200).json({
         data: token,
-        expired: 10000,
+        expired: 60000,
       });
     }
     // Check Password True & login End
